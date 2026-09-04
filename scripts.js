0e256f4534353195b9208c726e60856a31a80d92
potatoo <gh email>
2026-09-04 14:39:25 +0800

    variables ( let and const)

---
+++ scripts.js
@@ -0,0 +1,13 @@
+console.log("Hello, World!");
+console.log("from scripts.js file.");
+
+const myname = "jaysonnet";
+const number = "09123456789";
+const address = "Cebu City, Philippines";
+let myage = 20;
+let age = 16;
+
+console.log(`my name is ${myname} and my age is ${myage} years old.`);
+console.log(`my phone number is ${number}.`);
+console.log(`my address is ${address}.`);
+console.log(67);
